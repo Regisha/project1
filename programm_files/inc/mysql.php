f95bb52c0d9e82f88ce436aaffe2f5cbdcc46b21
Regina <9174844812@yandex.ru>
2018-12-26 10:52:45 +0500

    Удалила файл index
    он тестовый, не от этой системы

--- programm_files/inc/mysql.php
+++ programm_files/inc/mysql.php
@@ -4,18 +4,18 @@
 	die ("<meta http-equiv=refresh content='0; url=http://".$HTTP_HOST."/index.php'>");
 	}
 
-	$config['db_username'] = $userDB;
-	$config['db_password'] = $passDB;
-	$config['db_hostname'] = $hostDB;
-	$config['db_name'] = $baseDB;
-		
+	$config['db_username'] = ;
+	$config['db_password'] = ;
+	$config['db_hostname'] = ;
+	$config['db_name'] = ;
+
 	$mysql = mysqli_connect($hostDB,$userDB,$passDB,$baseDB);
-            
+
 	mysqli_set_charset($mysql, "utf8");
 
-	if (!$mysql) { 
-	printf("Невозможно подключиться к базе данных. Код ошибки: %s\n", mysqli_connect_error()); 
-	exit; 
+	if (!$mysql) {
+	printf("Невозможно подключиться к базе данных. Код ошибки: %s\n", mysqli_connect_error());
+	exit;
 	}
 
 ?>
